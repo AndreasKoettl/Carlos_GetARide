3a6d9f9db67a3f4f210017ebedba8206e04f7dc3
s1710238044 <32874952+s1710238044@users.noreply.github.com>
2019-05-01 16:40:59 +0200

    datetimepicker

---
+++ Carlos_GetARide/www/scripts/fahrt_suchen.js
@@ -0,0 +1,39 @@
+﻿"use strict";
+
+//var mtd280 = mtd280 || {};
+
+var options = {
+    type: 'date',         // 'date' or 'time', required
+    date: new Date(),     // date or timestamp, default: current date
+    minDate: new Date(),  // date or timestamp
+    maxDate: new Date()   // date or timestamp
+};
+
+function myfunction() {
+    window.DateTimePicker.pick(options, function (timestamp) {
+        window.alert(timestamp);
+    });
+}
+
+
+
+//mtd280.app = new Vue({ //{ => Object literal
+//    el: "#app",
+
+//    data: {
+      
+//    },
+
+//    methods: {
+//        hello: function(){
+//            event.preventDefault();
+//            console.log("HelloHello");
+//        },
+
+//        myfunction: function () {
+//            window.DateTimePicker.pick(options, function (timestamp) {
+//                window.alert(timestamp);
+//            });
+//        }
+//    }
+//});
