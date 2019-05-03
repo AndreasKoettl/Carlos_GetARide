e3d319532c1c48c76c67e521122952ed6746b98b
Johanna Geretschläger <s1710238024@students.fh-hagenberg.at>
2019-05-03 13:53:44 +0200

    fahrten aus db auslesen und auf seite einfügen funktioniert - bei der ersten Fahrt

--- Carlos_GetARide/www/php/load_rides.php
+++ Carlos_GetARide/www/php/load_rides.php
@@ -1,14 +1,33 @@
 ﻿<?php
 require_once '../lib/limonade-master/lib/limonade.php';
+require_once 'DatabaseAccess.php';
+require_once 'utilities.php';
+
+dispatch('/upcoming/:iduser', 'loadUpcomingRides');
 
 
-// just an example
-dispatch_post('/upcoming', 'hello');
-        function hello()
-        {
-            echo('hello');
-            return 'Hello world!';
-        }
+function loadUpcomingRides()
+{
+	// Datenbankverbindung aufbauen.
+	$dbConnection = new DatabaseAccess;
+
+    // User mit engegebener Email Adresse suchen.
+    $dbConnection->prepareStatement("SELECT * FROM drives WHERE users_idusers = :iduser");
+    $dbConnection->bindParam(":iduser", htmlentities(params("iduser"), ENT_QUOTES));
+    $dbConnection->executeStatement();
+    $result = $dbConnection->fetchAll();
+    // Prüfen ob User vorhanden ist, und das angegebene Passwort mit dem gespeicherten Hash übereinstimmt.
+    if ($dbConnection->getRowCount() > 0) {
+
+        $result = setSuccessMessage($result, "Ladevorgang erfolgreich.");
+    }
+    else {
+        $result = setErrorMessage($result, "Keine Fahrten vorhanden.");
+    }
+
+    // Userdaten und Statusnachrichten zurückgeben.
+    return json_encode($result);
+	
+}
+
 run();
-
-?>
--- Carlos_GetARide/www/scripts/meine_fahrten_script.js
+++ Carlos_GetARide/www/scripts/meine_fahrten_script.js
@@ -30,40 +30,70 @@ carlos_meineFahrten.app = new Vue({
         },
         
         loadUpcomingRides: function () {
+
             // get user id
             let iduser = JSON.parse(localStorage.getItem(STORAGE_KEY))["idusers"];
-            console.log(iduser);
-
+            let userData = JSON.stringify('{ "userid":' + iduser + '}');
 
             // AJAX-Post Request starten.
-            $.post({
+            $.ajax({
                 accepts: "application/json",
-                dataType: "json",
                 async: true,
                 contentType: false,
                 processData: false,
-                url: "/carlos/Carlos_GetARide/www/php/load_rides.php?/upcoming",
+                url: "/carlos/Carlos_GetARide/www/php/load_rides.php?/upcoming/" + iduser,
                 data: iduser,
                 success: function (data) {
+                    let result = JSON.parse(data);
                     // Prüfen ob das Anmelden erfolgreich war.
-                    //if (data["status"] === "success") {
-                        console.log(data["data"]);
-                    //}
-                    //else {
+                    if (result["status"] === "success") {
+
+                        // Ausgabe in Box
+
+                        let startDestination = document.getElementsByClassName("start-destination")[0];
+                        startDestination.innerHTML = result["data"][0]["locationStart"] + " - " + result["data"][0]["locationEnd"];
+                        
+                        let date = new Date(result["data"][0]["driveDate"]);
+                        let day = date.getDay();
+
+                        switch (day) {
+                            case 1: day = "Mo"; break;
+                            case 2: day = "Di"; break;
+                            case 3: day = "Mi"; break;
+                            case 4: day = "Do"; break;
+                            case 5: day = "Fr"; break;
+                            case 6: day = "Sa"; break;
+                            case 7: day = "So"; break;
+                            default: day = ""; break;
+                        }
+
+                        let dayNB = date.getDate();
+                        let month = date.getMonth();
+                        let year = date.getFullYear();
+
+                        let dateString = day + ", " + dayNB + "." + month + "." + year;
+
+                        let dateTime = document.getElementsByClassName("datetime")[0];
+                        dateTime.innerHTML = dateString + ", " + result["data"][0]["driveTime"];
+                        
+                    }
+
+                    else {
                         // Fehlermeldung ausgeben, wenn die Anmeldung nicht erfolgreich war.
-                        console.log("Login fehlgeschlagen: " + data["statusmessage"]);
-                    //}
+                        console.log("Laden fehlgeschlagen: " + result["statusmessage"]);
+                    }
                 },
                 error: function () {
                     console.log("Server Verbindung fehlgeschlagen.");
                 }
             });
         }
+
     },
 
     mounted: function () {
-        let ridesUpcoming = document.getElementById('rides-upcoming');
-        let ridesPast = document.getElementById('rides-past');
+        this.ridesUpcoming = document.getElementById('rides-upcoming');
+        this.ridesPast = document.getElementById('rides-past');
         
         this.loadUpcomingRides();
     }
