054be1563d97de3e3710425ea578bc83823e57a7
s1710238044 <32874952+s1710238044@users.noreply.github.com>
2019-06-12 10:56:21 +0200

    add vue and make dynamic

---
+++ Carlos_GetARide/www/php/profil.php
@@ -0,0 +1,12 @@
+﻿<?php
+require_once '../lib/limonade-master/lib/limonade.php';
+
+dispatch('loadUserData', 'loadUserData');
+
+function loadUserData(){
+
+}
+
+run();
+
+?>
---
+++ Carlos_GetARide/www/scripts/profile.js
@@ -0,0 +1,31 @@
+﻿"use strict";
+
+new Vue({
+    el: "#profilec",
+
+    data: {
+        profile: "true",
+        settings: "false",
+        edit: "false",
+        firstname: "",
+        lastname: "",
+        email: ""
+
+    },
+
+    methods: {
+        loadUser: function () {
+            this.profile = false;
+            this.settings = false;
+            this.firstname = "Myfirstname";
+            this.lastname= "Mylastname";
+            this.email = "mymail@mail.com";
+            console.log("hellomam");
+        }
+    },
+
+    mounted: function() {
+        console.log("dis go?");
+    }
+
+});
